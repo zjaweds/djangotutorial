4f6b59e60e0fc487906a0b786e8cb8463db76d78
Jawed Alam <zjaweds@outlook.com>
2025-05-03 03:26:31 +0530

    Complete tutorial

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
 
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path('admin/', admin.site.urls),
-]
+] + debug_toolbar_urls()
+
+
+# if not settings.TESTING:
+#     urlpatterns = [
+#         *urlpatterns,
+#     ] + debug_toolbar_urls()
